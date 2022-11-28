6b6172409bf07d0e13ccdce5d29d61d7b294720f
Jackie <heatleyj7@icloud.com>
2022-11-27 22:32:14 -0500

    worked on User.js

--- Develop/models/User.js
+++ Develop/models/User.js
@@ -2,30 +2,37 @@ const { Schema, model } = require('mongoose');
 const userSchema = require('./User');
 
 // Schema to create Thought model
-const thoughtSchema = new Schema(
+const userSchema = new Schema(
   {
-username: {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
+    },
+    email: {
       type: String,
       required: true,
-      max_length: 50,
+      unique: true,
+      // match: ask tutor
     },
-   email: {
-      type: String,
-      required: true,
-      max_length: 50,
+    thoughts: [{
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
     },
-   thoughts: {
-      type: String,
-      required: true,
-      max_length: 50,
+    ],
+    friends: [{
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-    friends: [assignmentSchema],
+    ],
   },
   {
     toJSON: {
       getters: true,
-    },
+    }
   }
+
 );
 
 const User = model('user', userSchema);
